61c6b7f3043938abd1a1a83045665d20e3253f24
Matsonj30 <matsonj2013@gmail.com>
2023-02-14 18:52:24 -0700

    removed printing

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ import os
 def convertToWebP(currentImage):
 
     destination = currentImage.with_suffix(".webp")
-    print(destination)
     openImage = Image.open(currentImage)
-    print(currentImage)
+
     openImage.save(destination, format="webp")
     os.remove(currentImage)
 
@@ -19,12 +18,8 @@ def main():
 
 
     for image in PNGimages:
-          print(image)
           convertToWebP(image)
-        # try:
-      
-        # except:
-        #     print("Exception occured")
+
 
     for image in JPGimages:
          convertToWebP(image)
